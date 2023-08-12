39c62130b87d983475db572f1036341b4a57fa23
Saidurga513 <saidurgak1998@gmail.com>
2023-08-12 16:57:24 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+  public  static void main(String arg[])
+  {
+      RWOnly obj=new RWOnly();
+     // obj.name="sai";
+      obj.setName("sai");
+      obj.getName();
+  }
 }
